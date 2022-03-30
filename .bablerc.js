ec98352ae61c337225a58c7b5edbaf8d9278e4a6
yunji <tndufqnswk@naver.com>
2022-03-30 15:27:37 +0900

    create webpack project

---
+++ .bablerc.js
@@ -0,0 +1,8 @@
+// babelrc.js
+
+module.exports = {
+    presets: [ '@babel/preset-env' ],
+    plugins: [
+        [ '@babel/plugin-transform-runtime' ]
+    ]
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+//js/main.js
+
+// import css from './css/main.css'
+import '../css/main.scss'
+
+console.log('WebPack!!3333');
